fd48699a7f0a12bfc550936bf28f9ee58a5b7d55
Thiago Aguiar <vinicius82thiago@yahoo.com.br>
2026-08-14 16:37:42 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const slides = document.querySelectorAll(".slide");
+
+let current = 0;
+
+function nextSlide(){
+
+    slides[current].classList.remove("active");
+
+    current++;
+
+    if(current >= slides.length){
+        current = 0;
+    }
+
+    slides[current].classList.add("active");
+}
+
+setInterval(nextSlide, 5000);
